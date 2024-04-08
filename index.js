d2a6696cf24957dbd5aba13129a8101ffef50192
lizwachira <lizwanguwachira@gmail.com>
2024-04-08 22:49:13 +0300

    dom-modifying elem

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+document.addEventListener("Creating and Inserting DOM Elements"() {
+    document.getElementById("text").textContent = "Liz Wachira is the champion"});
+
+
+
+const newHeader = document.createElement('main');
+newHeader.id = 'victory';
+newHeader.textContent = 'Liz Wachira is the champion';
+
+document.querySelector('main#main').remove(); //you can no longer see the text content
+
+document.body.appendChild(newHeader);
+//creating a new element in the web console
+const paragraph = document.createElement('test');
+newHeader.id = 'bodytest';
+//add the paragraph element to an existing element with the ID "test"
+document.getElementById("test").appendChild(paragraph);
